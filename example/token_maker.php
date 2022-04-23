29a144e7bbd968302d0002c107393f5c5417a055
AndrewGraber <graber15@purdue.edu>
2022-04-23 17:07:03 -0400

    fixed jquery import

--- example/token_maker.php
+++ example/token_maker.php
@@ -14,7 +14,7 @@ if(isset($_GET['temp_key'])) {
 <html>
 <head>
     <title>Example Token Maker</title>
-    <script type="text/javascript" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 
     <style>
